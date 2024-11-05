990c806a068b2695871b08d1b74fa8dd470dec30
DavidPiwow <46378076+DavidPiwow@users.noreply.github.com>
2024-11-05 05:25:43 -0500

    Update chess.c

--- chess.c
+++ chess.c
@@ -4,9 +4,9 @@
 #define BOARD_SIZE 8 
 
 struct _board {
-    int grid[BOARD_SIZE * BOARD_SIZE];
-    int* board_start;
-    int* board_end;
+    char grid[BOARD_SIZE * BOARD_SIZE];
+    char* board_start;
+    char* board_end;
 };
 
 struct _move {
@@ -15,29 +15,24 @@ struct _move {
 
 typedef struct _move Move;
 typedef struct _board Board;
-typedef int (*validation_function)(*Move);
+
+typedef int (*validation_function)(Move*);
 
 Board* create_board() {
     Board* temp = (Board*)malloc(sizeof(Board));
     temp->board_start = &temp->grid[0];
     temp->board_end = (temp->board_start + BOARD_SIZE * BOARD_SIZE);
-	char pieces_u[8] = ['C','N','B','Q','K','B','N','C']; 
-    char pieces_l[8] = ['c','n','b','q','k','b','n','c'];
-    int* cur_pos = temp->board_start;
-    int i = 1;
-    //int z = 10;
-    while (cur_pos != temp->board_end) {
-       *cur_pos = i; //(cur_pos - temp->board_start) % BOARD_SIZE + z;
-       cur_pos++;
-       i++;
-       //if (i++ % 10 == 0) z += 10;
-    }
-	
+	char pieces_u[8] = {'C','N','B','Q','K','B','N','C'}; 
+    char pieces_l[8] = {'c','n','b','q','k','b','n','c'};
+    char* cur_pos = temp->board_start;
+    memcpy(cur_pos, *pieces_u, sizeof(pieces_u));
+  
+    
     return temp;
 }
 
 void print_board(Board* grid) {
-    int* cur_pos = grid->board_start;
+    char* cur_pos = grid->board_start;
      while (cur_pos != grid->board_end) {
         printf("%2d ", *cur_pos);
         cur_pos++;
@@ -47,7 +42,7 @@ void print_board(Board* grid) {
     }
 }
 
-static inline int get_piece_at(Board* grid, int x, int y) {
+static inline char get_piece_at(Board* grid, int x, int y) {
     return *(grid->board_start + x + (y * BOARD_SIZE));
 }
 
@@ -100,6 +95,7 @@ void print_column(Board* grid, int column) {
         printf("%d\n", *(grid->board_start+i*BOARD_SIZE + column));
     }
 }
+
 
 int main() {
     Board* game_board = create_board();
